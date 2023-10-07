bb4c5ff8b0aa500969470a90b8b5474efb3331cf
boradoriii <syjoo0126@naver.com>
2023-10-07 16:50:29 +0900

    4

--- main.c
+++ main.c
@@ -7,10 +7,15 @@ int main(int argc, char *argv[]) {
 	
 	int a;
 	
-	printf("Input the second : ");
+	printf("Input the year : ");
 	scanf("%i", &a);
 	
-	printf("the time is %i : %i", a/60, a%60);
+	if ((a%4==0 && a%100!=0) || (a%400==0)){ // 4로 나누어 떨어지면서 100으로 나누어 떨어지지 않거나 400으로 나누어 떨어지면 윤년 
+		printf("is the year %i the leap year? : 1", a); // 윤년이면 1을 출력  
+	}
+	else {
+		printf("is the year %i the leap year? : 0", a); // 윤년이 아니면 0을 출력  
+	}
 	
 	return 0;
 }
